2720df89ea5c7f598632bcc6e6d1b3d24d427d03
Stephen Yeargin <stephen.yeargin@gmail.com>
2014-06-21 23:03:03 -0500

    Use Bower to manage javascript assets
    Reference: https://coderwall.com/p/6bmygq
    
    - The `rake stats` numbers were getting thrown off because of the javascript lines being counted against it
    - This puts the assets under management via `bower.json` instead (puts them in a `vendor/assets/components` folder)
    - In the process, switched out the Growl+Bootstrap library to one that is a little more flexible (and compatible with Bower)
    - :dollar:

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,20 @@ module SeatShare
 
     config.assets.precompile += %w[admin/active_admin.css admin/active_admin.js]
 
+    # Add the components path to the list of assets
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+
+    # We don't want the default of everything that isn't js or css, because it pulls too many things in
+    config.assets.precompile.shift
+
+    # Explicitly register the extensions we are interested in compiling
+    config.assets.precompile.push(Proc.new do |path|
+      File.extname(path).in? [
+        '.html', '.erb', '.haml',                 # Templates
+        '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
+        '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+      ]
+    end)
+
   end
 end
