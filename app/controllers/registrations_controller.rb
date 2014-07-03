8e5c17164f447e0831cdff1f029c968c66ffae94
Stephen Yeargin <stephen.yeargin@gmail.com>
2014-07-02 21:28:35 -0500

    Fix for invitation code being passed through registration process
    Needed to override the Devise registration controller a bit to pass data along if the user clicked an invitation link from an email. The code should not have to be re-entered after a successful signup if it was already provided in the query string.

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,34 @@
+class RegistrationsController < Devise::RegistrationsController
+  def new
+    @page_title = "Create an Account"
+    super
+  end
+
+  def create
+    build_resource(sign_up_params)
+
+    if resource.save
+      yield resource if block_given?
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        if params[:user][:invite_code]
+          respond_with resource, location: groups_join_path + "?invite_code=" + params[:user][:invite_code]
+        else
+          respond_with resource, location: after_sign_up_path_for(resource)
+        end
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+
+  def update
+    super
+  end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ SeatShare::Application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :admin_users, ActiveAdmin::Devise.config
 
-  devise_for :users, :path => '', :path_names => { :sign_in => 'login', :sign_out => 'logout', :sign_up => 'register' }
+  devise_for :users, :controllers => {:registrations => "registrations"}, :path => '', :path_names => { :sign_in => 'login', :sign_out => 'logout', :sign_up => 'register' }
 
   root 'public#index'
 
