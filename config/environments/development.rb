0d31183c6dea95c85f0a377eafa24221215563a2
Stephen Yeargin <stephen.yeargin@gmail.com>
2014-07-16 17:37:56 -0500

    They fixed their issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,8 +33,4 @@ SeatShare::Application.configure do
   config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
   config.action_mailer.smtp_settings = { :address => "127.0.0.1", :port => 1025 }
   config.action_mailer.asset_host = "http://localhost:3000"
-
-  # Disable SSL verification in development (dirty / remove me)
-  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-
 end
