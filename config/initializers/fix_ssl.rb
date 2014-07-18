d9970a5abd214197d0c6a36ca676ce75366ebb37
Stephen Yeargin <stephen.yeargin@gmail.com>
2014-07-17 20:37:47 -0500

    Try this.

--- config/initializers/fix_ssl.rb
+++ config/initializers/fix_ssl.rb
@@ -8,7 +8,8 @@ module Net
     alias_method :original_use_ssl=, :use_ssl=
 
     def use_ssl=(flag)
-      path = ( Rails.env == "development") ? "lib/ca-bundle.crt" : "/usr/lib/ssl/certs/ca-certificates.crt"
+      # path = ( Rails.env == "development") ? "lib/ca-bundle.crt" : "/usr/lib/ssl/certs/ca-certificates.crt"
+      path = "lib/ca-bundle.crt"
       self.ca_file = Rails.root.join(path).to_s
       self.verify_mode = OpenSSL::SSL::VERIFY_PEER
       self.original_use_ssl = flag
