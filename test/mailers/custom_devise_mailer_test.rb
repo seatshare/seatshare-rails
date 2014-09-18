ae54358692250d13c32c1f0c22454847f0bac895
Stephen Yeargin <stephen.yeargin@gmail.com>
2014-09-17 22:53:08 -0500

    Adds a test

--- test/mailers/custom_devise_mailer_test.rb
+++ test/mailers/custom_devise_mailer_test.rb
@@ -1,7 +1,52 @@
 require 'test_helper'
 
 class CustomDeviseMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    ActionMailer::Base.deliveries = []
+    Devise.mailer = 'CustomDeviseMailer'
+  end
+
+  def user
+    @user ||= begin
+      user = User.find(1)
+      user.send_reset_password_instructions
+      user
+    end
+  end
+
+  def mail
+    @mail ||= begin
+      user
+      ActionMailer::Base.deliveries.last
+    end
+  end
+
+  test 'email sent after reseting the user password' do
+    assert_not_nil mail
+  end
+
+  test 'content type should be set to html' do
+    assert mail.content_type.include?('text/html')
+  end
+
+  test 'send confirmation instructions to the user email' do
+    assert_equal [user.email], mail.to
+  end
+
+  test 'setup sender from configuration' do
+    assert_equal ['no-reply@myseatshare.com'], mail.from
+  end
+
+  test 'body should have user info' do
+    assert_match user.email, mail.body.encoded
+  end
+
+  test 'body should have link to confirm the account' do
+    if mail.body.encoded =~ %r{<a href=\"http://localhost:3000/password/edit\?reset_password_token=([^"]+)">}
+      assert_equal Devise.token_generator.digest(user.class, :reset_password_token, $1), user.reset_password_token
+    else
+      flunk "expected reset password url regex to match"
+    end
+  end
+
 end
