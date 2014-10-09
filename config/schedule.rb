3a9795c4d8b85f17b00bbe7c8bd7544734a3a3a3
Stephen Yeargin <stephen.yeargin@gmail.com>
2014-10-09 01:24:00 -0500

    fix whenever x2

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,11 +2,11 @@
 set :output, "/tmp/whenever.log"
 
 # Daily schedule
-every 1.day, :at => Time.zone.parse('4:30 am').utc do
+every 1.day, :at => '9:30 am' do
   rake "send_reminders:daily"
 end
 
 # Weekly schedule
-every :monday, :at => Time.zone.parse('4 am').utc do
+every :monday, :at => '9 am' do
   rake "send_reminders:weekly"
 end
