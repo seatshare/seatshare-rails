7cd1d2dabadf2a4b2b4b3b8eb0505421d2fb05d7
Stephen Yeargin <stephen.yeargin@gmail.com>
2014-10-09 01:19:03 -0500

    fix whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,6 @@
 # Log things
 set :output, "/tmp/whenever.log"
 
-# Set for easier management
-Time.zone = "US/Central"
-
 # Daily schedule
 every 1.day, :at => Time.zone.parse('4:30 am').utc do
   rake "send_reminders:daily"
