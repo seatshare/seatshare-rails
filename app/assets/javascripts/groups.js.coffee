seatshare/seatshare-rails

commit 66986fd24b63bb3ebcf368f210959499631aaa0f

Formatting/fixing for Create Group page
- Fixes #97
- Handles empty value posts better
- Reverses order of picking a team then naming the group
- Adds a bit of interactivity to suggest a group name
- Adds a little bit of promo copy to the right highlighting available leagues
- Rewords promo text at the top

![screenshot](http://cl.ly/image/0o2o03051B1i/Screen%20Shot%202014-10-11%20at%202.43.23%20PM.png)

app/assets/javascripts/groups.js.coffee
@@ -6,7 +6,17 @@ groupsReady = ->
 
   # Create Group
   if $("#group_entity_id").length > 0
-    $("#group_entity_id").select2()
+    $("#group_entity_id").select2({
+      placeholder: "Click to search our 2,200 available teams",
+      alloweClear: true
+    })
+
+  # Change Placeholder Text
+  $("#group_entity_id").on "select2-selected", ->
+    data = $("#group_entity_id").select2 "data"
+    suffixes = ['Fans', 'Super Fans', 'Supporters', 'Backers', 'Crew', 'Pride']
+    suffix = suffixes[Math.floor(Math.random() * suffixes.length)];
+    $('#group_group_name').attr 'placeholder', "e.g. #{data.text} #{suffix}"
 
   # Calendar
   group_id = $('#group_selector').val()

app/controllers/groups_controller.rb
@@ -32,6 +32,10 @@ class GroupsController < ApplicationController
       flash.keep
       flash[:notice] = "Group created!"
       redirect_to :action => 'show', :id => @group.id
+    else
+      flash.keep
+      flash[:error] = "Group could not be created."
+      redirect_to :action => 'new', :entity_id => params[:entity_id]
     end
   end
 

app/views/groups/new.html.erb
@@ -3,24 +3,25 @@
 <fieldset>
   <legend>Group Details</legend>
 
-  <p class="lead">Groups of users exchange tickets among its membership. You can assign tickets to other users, or request their available tickets.</p> 
+  <p class="lead">A ticket group exchanges tickets among its membership. You can assign tickets to other users, or request their available tickets.</p> 
 
   <div class="form-group">
-    <%= f.label :group_name, :class => 'col-md-3 control-label' %>
-    <div class="col-md-9">
-      <%= f.text_field :group_name, :class => 'form-control', :placeholder => 'Cellblock 303' %>
-    </div>
-  </div>
-  <div class="form-group">
-    <%= f.label :entity_id, :class => 'col-md-3 control-label'  %>
+    <%= f.label 'Your team', :class => 'col-md-3 control-label'  %>
     <div class="col-md-9">
       <% if @entity %>
       <p class="form-control-static"><%= @entity.display_name %></p>
       <%= f.hidden_field :entity_id, :value => @entity.id %>
       <% else %>
-      <%= select_tag 'group[entity_id]', grouped_options_for_select(@entities), { :class => 'form-control' } %>
+      <%= select_tag 'group[entity_id]', grouped_options_for_select(@entities), { :class => 'form-control', :prompt => '' } %>
       <p class="help-block"><strong>Choose carefully!</strong> You cannot change your team / venue after the group is created.</p>
       <% end %>
+    </div>
+  </div>
+
+  <div class="form-group">
+    <%= f.label :group_name, :class => 'col-md-3 control-label' %>
+    <div class="col-md-9">
+      <%= f.text_field :group_name, :class => 'form-control input-lg', :placeholder => 'e.g. Your Team Super Fans' %>
     </div>
   </div>
 
@@ -30,6 +31,27 @@
   <div class="text-right col-md-12">
     <%= f.submit 'Create Group', :class => 'btn btn-primary' %>
   </div>
+</div>
+
+<br />
+
+<% end %>
+
+<% content_for :sidebar do %>
+
+<div class="well hidden-xs">
+  <h3>All of your favorite teams</h3>
+  <ul>
+    <li>National Football League</li>
+    <li>Major League Baseball</li>
+    <li>National Basketball Association</li>
+    <li>National Hockey League</li>
+    <li>College Football</li>
+    <li>College Basketball</li>
+    <li>Major League Soccer</li>
+    <li>Canadian Football League</li>
+    <li>Women's Flat Track Roller Derby</li>
+  </ul>
 </div>
 
 <% end %>

commit e9338ead257ef531f0598af553a4351831fd7adf

Fix alloweClear error in groups.js.coffee
Introduced in 66986fd. :panda_face:

app/assets/javascripts/groups.js.coffee
@@ -8,7 +8,7 @@ groupsReady = ->
   if $("#group_entity_id").length > 0
     $("#group_entity_id").select2({
       placeholder: "Click to search our 2,200 available teams",
-      alloweClear: true
+      allowClear: true
     })
 
   # Change Placeholder Text