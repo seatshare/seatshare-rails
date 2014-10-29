0080633c828fea8895c82492d639c7869d1cfb25
Stephen Yeargin <stephen.yeargin@gmail.com>
2014-10-29 16:54:07 -0500

    Add one more intermittent failure test
    Related to #109

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -17,7 +17,10 @@ class EventTest < ActiveSupport::TestCase
     assert event.start_time.day == 1, 'new event day matches'
     assert event.date_tba? === false, 'new event date is not TBA'
     assert event.time_tba? === false, 'new event time is not TBA'
-    assert event.date_time === 'Tuesday, January 1, 2013 - 6:00 pm CST', 'new event date/time string matches'
+    fails_intermittently('https://github.com/seatshare/seatshare-rails/issues/109',
+      'Rails.configuration.time_zone' => Rails.configuration.time_zone, 'Time.zone.name' => Time.zone.name, 'user.timezone' => user.timezone) do
+      assert event.date_time === 'Tuesday, January 1, 2013 - 6:00 pm CST', 'new event date/time string matches'
+    end
   end
 
   test "fixture event has attributes" do
