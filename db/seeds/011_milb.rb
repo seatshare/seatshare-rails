41f2e625e52dc292cdeefc6ab6cbeb27eafe5812
Stephen Yeargin <stephen.yeargin@gmail.com>
2015-03-09 00:54:53 -0500

    Add Minor League Baseball Teams
    - Adds MiLB teams (as of ~2012, there are some known moves not accounted for)

---
+++ db/seeds/011_milb.rb
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+Entity.create([
+  {
+    id: 4241, import_key: "l.ilbaseball.com-t.325", entity_name: "Scranton/Wilkes-Barre Yankees", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4242, import_key: "l.ilbaseball.com-t.318", entity_name: "Lehigh Valley IronPigs", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4243, import_key: "l.ilbaseball.com-t.319", entity_name: "Pawtucket Red Sox", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4244, import_key: "l.ilbaseball.com-t.322", entity_name: "Rochester Red Wings", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4245, import_key: "l.ilbaseball.com-t.300", entity_name: "Buffalo Bisons", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4246, import_key: "l.ilbaseball.com-t.326", entity_name: "Syracuse Chiefs", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4247, import_key: "l.ilbaseball.com-t.321", entity_name: "Gwinnett Braves", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4248, import_key: "l.ilbaseball.com-t.305", entity_name: "Durham Bulls", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4249, import_key: "l.ilbaseball.com-t.302", entity_name: "Charlotte Knights", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4250, import_key: "l.ilbaseball.com-t.315", entity_name: "Norfolk Tides", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4251, import_key: "l.ilbaseball.com-t.304", entity_name: "Columbus Clippers", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4252, import_key: "l.ilbaseball.com-t.311", entity_name: "Louisville Bats", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4253, import_key: "l.ilbaseball.com-t.328", entity_name: "Toledo Mud Hens", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4254, import_key: "l.ilbaseball.com-t.308", entity_name: "Indianapolis Indians", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4255, import_key: "l.pclbaseball.com-t.317", entity_name: "Omaha Storm Chasers", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4256, import_key: "l.pclbaseball.com-t.309", entity_name: "Iowa Cubs", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4257, import_key: "l.pclbaseball.com-t.312", entity_name: "Memphis Redbirds", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4258, import_key: "l.pclbaseball.com-t.313", entity_name: "Nashville Sounds", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4259, import_key: "l.pclbaseball.com-t.422", entity_name: "Round Rock Express", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4260, import_key: "l.pclbaseball.com-t.301", entity_name: "Albuquerque Isotopes", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4261, import_key: "l.pclbaseball.com-t.314", entity_name: "New Orleans Zephyrs", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4262, import_key: "l.pclbaseball.com-t.316", entity_name: "Oklahoma City RedHawks", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4263, import_key: "l.pclbaseball.com-t.329", entity_name: "Reno Aces", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4264, import_key: "l.pclbaseball.com-t.324", entity_name: "Salt Lake Bees", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4265, import_key: "l.pclbaseball.com-t.303", entity_name: "Colorado Springs Sky Sox", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4266, import_key: "l.pclbaseball.com-t.327", entity_name: "Tacoma Rainiers", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4267, import_key: "l.pclbaseball.com-t.323", entity_name: "Sacramento River Cats", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4268, import_key: "l.pclbaseball.com-t.307", entity_name: "Fresno Grizzlies", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4269, import_key: "l.pclbaseball.com-t.ZY1", entity_name: "Tucson Padres", entity_type_id: 11, status: 1
+  },
+  {
+    id: 4270, import_key: "l.pclbaseball.com-t.310", entity_name: "Las Vegas 51s", entity_type_id: 11, status: 1
+  }
+])
