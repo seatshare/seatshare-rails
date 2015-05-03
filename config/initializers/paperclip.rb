a97a9443dad399db1ab710bdfd2418d5d1f8c67d
Stephen Yeargin <stephen.yeargin@gmail.com>
2015-05-02 19:54:03 -0500

    Set default group icon based on entity type
    The default ticket image is fine, but it's kind of irrelevant for a new user (and most are not changing it for their group). This PR adds a series of public domain clipart images for the various leagues to give the group a more personal feel. Users are still able to override the default image with their team or group logo.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,10 @@
 # Group
-Paperclip.interpolates(:placeholder_group) do |attachment, style|
-  ActionController::Base.helpers.asset_path("group-#{style}-missing.png")
+Paperclip.interpolates :placeholder_group do |attachment, style|
+  eta = attachment.instance.entity.entity_type.entity_type_abbreviation.downcase
+  image_path = File.join 'entity_types', "#{eta}-group-#{style}-missing.png"
+  if File.exists?(File.join(Rails.root, 'app', 'assets', 'images', image_path))
+    ActionController::Base.helpers.asset_path image_path
+  else
+    ActionController::Base.helpers.asset_path("group-#{style}-missing.png")
+  end
 end
