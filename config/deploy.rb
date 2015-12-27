987bcec45ce6232c9f14896ed5bb24aaab9fbf0b
Stephen Yeargin <stephen.yeargin@gmail.com>
2015-12-26 21:57:39 -0600

    [Trivial] Re-lock cap version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-# config valid only for Capistrano 3.2.1
-lock '3.2.1'
+# config valid only for Capistrano 3.4.0
+lock '3.4.0'
 
 set :application, 'seatshare'
 set :repo_url, 'git@github.com:stephenyeargin/seatshare-rails.git'
