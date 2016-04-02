b9a09220a4a5e7b09e6eda3112c12d2d4934a2fa
Stephen Yeargin <stephen.yeargin@gmail.com>
2016-04-02 00:02:53 -0500

    Add Minor League Hockey seeds
    Adding these, mostly for next season, now that we have a reliable data source.

---
+++ db/seeds/014_echl.rb
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+entity_type_id = EntityType.find_by_entity_type_abbreviation('ECHL').id
+Entity.create([
+  {
+    entity_name: 'Adirondack Thunder', import_key: 'https://api.seatgeek.com/2/events?performers.slug=alaska-aces&venue.id=1389', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Alaska Aces', import_key: 'https://api.seatgeek.com/2/events?performers.slug=alaska-aces&venue.id=5531', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Allen Americans', import_key: '​https://api.seatgeek.com/2/events?performers.slug=allen-americans&venue.id=1416', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Atlanta Gladiators', import_key: 'https://api.seatgeek.com/2/events?performers.slug=gwinnett-gladiators&venue.id=6847', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Brampton Beast', import_key: 'https://api.seatgeek.com/2/events?performers.slug=brampton-beast&venue.id=1041', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Cincinnati Cyclones', import_key: 'https://api.seatgeek.com/2/events?performers.slug=cincinnati-cyclones&venue.id=210', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Colorado Eagles', import_key: 'https://api.seatgeek.com/2/events?performers.slug=colorado-eagles&venue.id=508', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Elmira Jackals', import_key: 'https://api.seatgeek.com/2/events?performers.slug=elmira-jackals&venue.id=1360', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Evansville Icemen', import_key: 'https://api.seatgeek.com/2/events?performers.slug=evansville-icemen&venue.id=6370', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Florida Everblades', import_key: 'https://api.seatgeek.com/2/events?performers.slug=florida-everblades&venue.id=951', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Fort Wayne Komets', import_key: 'https://api.seatgeek.com/2/events?performers.slug=fort-wayne-komets&venue.id=331', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Greenville Swamp Rabbits', import_key: 'https://api.seatgeek.com/2/events?performers.slug=greenville-road-warriors&venue.id=111', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Idaho Steelheads', import_key: 'https://api.seatgeek.com/2/events?performers.slug=idaho-steelheads&venue.id=3706', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Indy Fuel', import_key: 'https://api.seatgeek.com/2/events?performers.slug=indy-fuel&venue.id=1607', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Kalamazoo Wings', import_key: 'https://api.seatgeek.com/2/events?performers.slug=kalamazoo-wings&venue.id=3674', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Manchester Monarchs', import_key: 'https://api.seatgeek.com/2/events?performers.slug=manchester-monarchs&venue.id=806', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Missouri Mavericks', import_key: 'https://api.seatgeek.com/2/events?performers.slug=missouri-mavericks&venue.id=1445', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Norfolk Admirals', import_key: 'https://api.seatgeek.com/2/events?performers.slug=norfolk-admirals&venue.id=4608', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Orlando Solar Bears', import_key: 'https://api.seatgeek.com/2/events?performers.slug=orlando-solar-bears&venue.id=2652', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Quad City Mallards', import_key: 'https://api.seatgeek.com/2/events?performers.slug=quad-city-mallards&venue.id=95', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Rapid City Rush', import_key: 'https://api.seatgeek.com/2/events?performers.slug=rapid-city-rush-1&venue.id=102', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Reading Royals', import_key: 'https://api.seatgeek.com/2/events?performers.slug=reading-royals&venue.id=629', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'South Carolina Stingrays', import_key: 'https://api.seatgeek.com/2/events?performers.slug=south-carolina-stingrays&venue.id=355', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Toledo Walleye', import_key: 'https://api.seatgeek.com/2/events?performers.slug=toledo-walleye&venue.id=2541', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Tulsa Oilers', import_key: 'https://api.seatgeek.com/2/events?performers.slug=tulsa-oilers&venue.id=85', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Utah Grizzlies', import_key: 'https://api.seatgeek.com/2/events?performers.slug=utah-grizzlies&venue.id=3065', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Wheeling Nailers', import_key: 'https://api.seatgeek.com/2/events?performers.slug=wheeling-nailers&venue.id=2068', entity_type_id: entity_type_id, status: 1
+  },
+  {
+    entity_name: 'Wichita Thunder', import_key: 'https://api.seatgeek.com/2/events?performers.slug=wichita-thunder&venue.id=1568', entity_type_id: entity_type_id, status: 1
+  }
+])
