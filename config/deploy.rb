8f6ad3a07eca7fa88bbaed94352fc87a9bdc1695
Stephen Yeargin <stephen.yeargin@gmail.com>
2016-08-27 18:59:46 -0500

    [Trivial] Allow capistrano to use 3.4.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-# config valid only for Capistrano 3.4.0
-lock '3.4.0'
+# config valid only for Capistrano 3.4.1
+lock '3.4.1'
 
 set :application, 'seatshare'
 set :repo_url, 'git@github.com:stephenyeargin/seatshare-rails.git'
